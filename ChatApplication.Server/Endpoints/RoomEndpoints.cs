9487b9c4a8225bca84033bfcfbf8f6463ea95a20
Rami Obaid <163041066+RamiObaid92@users.noreply.github.com>
2025-10-09 20:11:45 +0200

    Add IsPrivate to ChatRoomEntity and Rooms API endpoint

---
+++ ChatApplication.Server/Endpoints/RoomEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using ChatApplication.Server.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApplication.Server.Endpoints;
+
+public static class RoomEndpoints
+{
+    public static void MapRoomEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/api/rooms");
+
+        group.MapGet("/", async (ApplicationDbContext db) =>
+        {
+            var rooms = await db.ChatRooms
+            .Select(r => new
+            {
+                r.Id,
+                r.Name
+            }).ToListAsync();
+
+            return Results.Ok(rooms);
+        });
+    }
+}
